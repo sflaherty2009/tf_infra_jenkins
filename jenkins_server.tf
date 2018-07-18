7e95052d9864798da31e1d0ebf6b3f5cc887be31
dreweasland <drew_easland@trekbikes.com>
2018-07-18 11:31:13 -0500

    Updates for DVO-3274

--- jenkins_server.tf
+++ jenkins_server.tf
@@ -1,38 +1,40 @@
 # Create resource group that will be used with Jenkins deploy
 resource "azurerm_resource_group" "jenkins" {
-  name     = "${var.resource_group_name}"
+  name     = "AZ-RG-${upper(terraform.workspace)}-JNKS"
   location = "${var.location}"
 }
 
 # Create public IPs
 resource "azurerm_public_ip" "jenkins" {
-  name                         = "${var.computer_name}-pubip"
+  name                         = "azl-${terraform.workspace}-jnks-${format("%02d", count.index+1)}-pubip"
   location                     = "${azurerm_resource_group.jenkins.location}"
   resource_group_name          = "${azurerm_resource_group.jenkins.name}"
   public_ip_address_allocation = "static"
+  count                        = "${lookup(var.count_jenkins_vms,terraform.workspace)}"
 }
 
 # Create virtual NIC that will be used with our Jenkins instance.
 resource "azurerm_network_interface" "jenkins" {
-  name                = "${azurerm_resource_group.jenkins.name}-nic"
-  location            = "${var.location}"
+  name                = "azl-${terraform.workspace}-jnks-${format("%02d", count.index+1)}-nic"
+  location            = "${azurerm_resource_group.jenkins.location}"
   resource_group_name = "${azurerm_resource_group.jenkins.name}"
+  count               = "${lookup(var.count_jenkins_vms,terraform.workspace)}"
 
   ip_configuration {
-    name                          = "${var.computer_name}-ipconf"
-    subnet_id                     = "${var.subnet_id}"
+    name                          = "azl-${terraform.workspace}-jnks-${format("%02d", count.index+1)}-ipconf"
+    subnet_id                     = "${lookup(var.subnet_id,terraform.workspace)}"
     private_ip_address_allocation = "dynamic"
     public_ip_address_id          = "${element(azurerm_public_ip.jenkins.*.id, count.index)}"
   }
 }
 
 resource "azurerm_storage_account" "jenkins" {
-  name                     = "azldevjenkin${format("%02d", count.index+1)}s"
+  name                     = "azl${terraform.workspace}jnks${format("%02d", count.index+1)}s"
   resource_group_name      = "${azurerm_resource_group.jenkins.name}"
   location                 = "${azurerm_resource_group.jenkins.location}"
   account_tier             = "Standard"
   account_replication_type = "LRS"
-  count                    = "${var.count_jenkins_vms}"
+  count                    = "${lookup(var.count_jenkins_vms,terraform.workspace)}"
 }
 
 resource "azurerm_storage_container" "jenkins" {
@@ -40,18 +42,24 @@ resource "azurerm_storage_container" "jenkins" {
   resource_group_name   = "${azurerm_resource_group.jenkins.name}"
   storage_account_name  = "${element(azurerm_storage_account.jenkins.*.name, count.index)}"
   container_access_type = "private"
-  count                 = "${var.count_jenkins_vms}"
+  count                 = "${lookup(var.count_jenkins_vms,terraform.workspace)}"
 }
 
 resource "azurerm_virtual_machine" "jenkins" {
-  name                  = "${var.computer_name}"
-  location              = "${var.location}"
+  name                  = "azl-${terraform.workspace}-jnks-${format("%02d", count.index+1)}"
+  location              = "${azurerm_resource_group.jenkins.location}"
   resource_group_name   = "${azurerm_resource_group.jenkins.name}"
-  network_interface_ids = ["${azurerm_network_interface.jenkins.id}"]
-  vm_size               = "${var.vm_size}"
+  network_interface_ids = ["${element(azurerm_network_interface.jenkins.*.id, count.index)}"]
+  vm_size               = "${lookup(var.vm_size,terraform.workspace)}"
+  count                 = "${lookup(var.count_jenkins_vms,terraform.workspace)}"
 
   delete_os_disk_on_termination    = true
   delete_data_disks_on_termination = true
+
+  provisioner "local-exec" {
+    when    = "destroy"
+    command = "knife node delete azl-${terraform.workspace}-jnks-${format("%02d", count.index+1)}-${azurerm_resource_group.jenkins.name} -y; knife client delete azl-${terraform.workspace}-jnks-${format("%02d", count.index+1)}-${azurerm_resource_group.jenkins.name} -y"
+  }
 
   storage_image_reference {
     publisher = "${var.lin_image_publisher}"
@@ -76,7 +84,7 @@ resource "azurerm_virtual_machine" "jenkins" {
   }
 
   os_profile {
-    computer_name  = "${var.computer_name}"
+    computer_name  = "azl-${terraform.workspace}-jnks-${format("%02d", count.index+1)}"
     admin_username = "${local.admin_user}"
     admin_password = "${local.admin_password}"
   }
@@ -84,31 +92,27 @@ resource "azurerm_virtual_machine" "jenkins" {
   os_profile_linux_config {
     disable_password_authentication = false
   }
-
-  provisioner "local-exec" {
-    when    = "destroy"
-    command = "knife node delete ${var.computer_name} -y; knife client delete ${var.computer_name} -y"
-  }
 }
 
 resource "azurerm_virtual_machine_extension" "jenkins" {
   name                       = "ChefClient"
-  location                   = "${var.location}"
+  location                   = "${azurerm_resource_group.jenkins.location}"
   resource_group_name        = "${azurerm_resource_group.jenkins.name}"
-  virtual_machine_name       = "${azurerm_virtual_machine.jenkins.name}"
+  virtual_machine_name       = "${element(azurerm_virtual_machine.jenkins.*.name, count.index)}"
   publisher                  = "Chef.Bootstrap.WindowsAzure"
   type                       = "LinuxChefClient"
   type_handler_version       = "1210.12"
   auto_upgrade_minor_version = true
+  count                      = "${lookup(var.count_jenkins_vms,terraform.workspace)}"
 
   settings = <<SETTINGS
   {
     "client_rb": "ssl_verify_mode :verify_none",
     "bootstrap_version": "${var.chef_client_version}",
     "bootstrap_options": {
-      "chef_node_name": "${var.computer_name}",
+      "chef_node_name": "${element(azurerm_virtual_machine.jenkins.*.name, count.index)}-${azurerm_resource_group.jenkins.name}",
       "chef_server_url": "${var.chef_server_url}",
-      "environment": "${var.chef_environment}",
+      "environment": "${lookup(var.chef_environment,terraform.workspace)}",
       "validation_client_name": "${var.chef_user_name}"
     },
     "runlist": "${var.chef_runlist}"
